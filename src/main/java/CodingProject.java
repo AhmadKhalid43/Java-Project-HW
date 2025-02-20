ccbf688903b82e7f0441e0f53b6918dcfd2f4978
Ahmad Khalid Saleh <aksaleh43@gmail.com>
2025-02-19 23:12:14 -0800

    first commit

---
+++ src/main/java/CodingProject.java
@@ -0,0 +1,23 @@
+public class CodingProject {
+
+    public static void main(String[] args) {
+
+
+        int[] numbers = {2, 4, 12, 8, 9, 14, 54, 79, 20, 7, 3, 11};
+
+            int sum=0;
+
+        for (int num:numbers) {
+
+            sum=sum+num;
+
+            System.out.print(sum+" ls");
+        }
+
+
+    }
+}
+
+
+
+
